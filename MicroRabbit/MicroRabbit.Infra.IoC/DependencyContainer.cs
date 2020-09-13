e6e607434af810b2375f7cecb52ba227047f4a06
TRAN Thai hung <htran1@mantu.com>
2020-09-13 17:23:40 +0700

    Partially complete microservice banking app

--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,4 +1,9 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -7,11 +12,17 @@ using System.Text;
 
 namespace MicroRabbit.Infra.IoC
 {
-    public class DependencyContainer
+    public static class DependencyContainer
     {
         public static void RegisterServices(IServiceCollection services)
         {
-            services.AddTransient<IEventBus, RabbitMQBus>();
+           // services.AddTransient<IEventBus, RabbitMQBus>();
+            //Application
+            services.AddTransient<IAccountService, AccountService>();
+
+            // Data
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<BankingDbContext>();
         }
     }
 }
